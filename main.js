3af98c681bca1cd1aa9f0cfd3eb7e59296c81944
Kojappa <85250592+Kojappa@users.noreply.github.com>
2021-06-03 01:52:10 +0800

    new site

---
+++ main.js
@@ -0,0 +1,66 @@
+$(document).ready(function() {
+    
+    var header = $("#header"),
+        introH = $("#intro").innerHeight(),
+        scrollOffset = $(window).scrollTop();
+       
+    /*** Fixed Header ***/
+    
+    checkScroll(scrollOffset);
+    
+    
+    $(window).on("scroll", function() {
+        
+        scrollOffset = $(this).scrollTop();
+        checkScroll(scrollOffset);
+    })
+    
+    function checkScroll(scrollOffset) {
+        
+        if ( scrollOffset >= introH) {
+            header.addClass("fixed");
+        }
+        else {
+            header.removeClass("fixed");
+        }
+    }
+    
+    /* Smoth Scroll*/
+    
+    $("[data-scroll]").on("click", function(event){
+        event.preventDefault();
+        
+        var blockid = $(this).data('scroll'),
+            blockOffset = $(blockid).offset().top;
+        
+        $("html, body").animate({
+            scrollTop: blockOffset
+        }, 500)
+    })
+    
+    /*Slider*/
+    
+    $('.slider').slick({
+        dots: true,
+        slidesToShow: 5,
+        slidesToScroll: 5,
+        infinite: true        
+    });
+    
+	/*** E-mail Ajax Send***/ 
+	$("form").submit(function() { //Change
+		var th = $(this);
+		$.ajax({
+			type: "POST",
+			url: "mail.php", //Change
+			data: th.serialize()
+		}).done(function() {
+			alert("Thank you!");
+			setTimeout(function() {
+				// Done Functions
+				th.trigger("reset");
+			}, 1000);
+		});
+		return false;
+	});    
+});
